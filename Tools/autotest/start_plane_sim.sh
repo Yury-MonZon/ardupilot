413b40800b2871695bc2bafce6606e74d177e7b8
Yury MonZon <yurymonzon@gmail.com>
2024-11-29 01:48:40 +0100

    MZ Targets

---
+++ Tools/autotest/start_plane_sim.sh
@@ -0,0 +1,2 @@
+./sim_vehicle.py -v ArduPlane --console --map --osd -D -G
+
---
+++ make_sbf405wmini_upload.sh
@@ -0,0 +1,2 @@
+./waf configure --board SpeedyBeeF405WING && ./waf -j32 plane && cp ./build/SpeedyBeeF405WING/bin/arduplane.apj ~/KVM_Share/Ardupilot_tests/SB405WING_MZ/ && ./waf --targets bin/arduplane --upload
+
