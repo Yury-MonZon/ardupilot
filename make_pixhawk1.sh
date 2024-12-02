549dffd127ee19abb3e69b73be54746814d93e2e
Yury MonZon <yurymonzon@gmail.com>
2024-12-02 23:34:05 +0100

    MZ Targets update

--- make_pixhawk1.sh
+++ make_pixhawk1.sh
@@ -1,2 +1,2 @@
-./waf configure --board Pixhawk1 && ./waf -j32 plane && cp ./build/Pixhawk1/bin/arduplane.apj ~/KVM_Share/Ardupilot_tests/Pixhawk1/
+./waf configure --board Pixhawk1-bdshot && ./waf -j32 plane
 
