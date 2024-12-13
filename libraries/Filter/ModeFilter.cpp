6c9c842e339911cabe05e9c8831cfd4df86ded9b
Yury MonZon <yurymonzon@gmail.com>
2024-12-13 09:19:57 +0100

    Better FF filtering

--- libraries/Filter/ModeFilter.cpp
+++ libraries/Filter/ModeFilter.cpp
@@ -97,6 +97,7 @@ void ModeFilter<T,FILTER_SIZE>::isort(T new_sample, bool drop_high)
 
 // instantiate required implementations
 template class ModeFilter<float,5>;
+template class ModeFilter<float,7>;
 template class ModeFilter<int16_t,3>;
 template class ModeFilter<int16_t,5>;
 template class ModeFilter<uint16_t,3>;
