5bf0f95d36735bb5adb977ed57825dcd1dce770c
Yury MonZon <yurymonzon@gmail.com>
2025-04-02 08:16:14 +0200

    Better transmitter tuning

--- ArduPlane/tuning.cpp
+++ ArduPlane/tuning.cpp
@@ -37,6 +37,12 @@ const uint8_t AP_Tuning_Plane::tuning_set_az[] =               { TUNING_AZ_P, TU
 const uint8_t AP_Tuning_Plane::tuning_set_rate_pitchDP[]=      { TUNING_RATE_PITCH_D, TUNING_RATE_PITCH_P };
 const uint8_t AP_Tuning_Plane::tuning_set_rate_rollDP[]=       { TUNING_RATE_ROLL_D, TUNING_RATE_ROLL_P };
 const uint8_t AP_Tuning_Plane::tuning_set_rate_yawDP[]=        { TUNING_RATE_YAW_D, TUNING_RATE_YAW_P };
+const uint8_t AP_Tuning_Plane::tuning_set_dp_roll_pitch[] =    { TUNING_RLL_D, TUNING_RLL_P, TUNING_PIT_D, TUNING_PIT_P };
+const uint8_t AP_Tuning_Plane::tuning_set_pidff_roll[] =       { TUNING_RLL_P, TUNING_RLL_I, TUNING_RLL_D, TUNING_RLL_FF };
+const uint8_t AP_Tuning_Plane::tuning_set_pidff_pitch[] =      { TUNING_PIT_P, TUNING_PIT_I, TUNING_PIT_D, TUNING_PIT_FF };
+// const uint8_t AP_Tuning_Plane::tuning_set_pidff_yaw[] =        { TUNING_YAW_P, TUNING_YAW_I, TUNING_YAW_D, TUNING_YAW_FF };
+// const uint8_t AP_Tuning_Plane::tuning_set_pidff_rpy[] =        { TUNING_RLL_P, TUNING_RLL_I, TUNING_RLL_D, TUNING_RLL_FF, TUNING_PIT_P, TUNING_PIT_I, TUNING_PIT_D, TUNING_PIT_FF, TUNING_YAW_P, TUNING_YAW_I, TUNING_YAW_D, TUNING_YAW_FF };
+const uint8_t AP_Tuning_Plane::tuning_set_pid_rp[] =          { TUNING_RLL_P, TUNING_RLL_I, TUNING_RLL_D, TUNING_PIT_P, TUNING_PIT_I, TUNING_PIT_D };
 
 // macro to prevent getting the array length wrong
 #define TUNING_ARRAY(v) ARRAY_SIZE(v), v
@@ -53,6 +59,12 @@ const AP_Tuning_Plane::tuning_set AP_Tuning_Plane::tuning_sets[] = {
     { TUNING_SET_RATE_PITCHDP,          TUNING_ARRAY(tuning_set_rate_pitchDP) },
     { TUNING_SET_RATE_ROLLDP,           TUNING_ARRAY(tuning_set_rate_rollDP) },
     { TUNING_SET_RATE_YAWDP,            TUNING_ARRAY(tuning_set_rate_yawDP) },
+    { TUNING_SET_DP_ROLL_PITCH,         TUNING_ARRAY(tuning_set_dp_roll_pitch) },
+    { TUNING_SET_PIDFF_ROLL,            TUNING_ARRAY(tuning_set_pidff_roll) },
+    { TUNING_SET_PIDFF_PITCH,           TUNING_ARRAY(tuning_set_pidff_pitch) },
+    // { TUNING_SET_PIDFF_YAW,             TUNING_ARRAY(tuning_set_pidff_yaw) },
+    // { TUNING_SET_PIDFF_RPY,             TUNING_ARRAY(tuning_set_pidff_rpy) },
+    { TUNING_SET_PID_RP,                TUNING_ARRAY(tuning_set_pid_rp) },
     { 0, 0, nullptr }
 };
 
@@ -95,6 +107,10 @@ const AP_Tuning_Plane::tuning_name AP_Tuning_Plane::tuning_names[] = {
     { TUNING_PIT_D,        "PitchD" },
     { TUNING_PIT_FF,       "PitchFF" },
     { TUNING_Q_FWD_THR,    "QModeFwdThr" },
+    // { TUNING_YAW_P,        "YawP" },
+    // { TUNING_YAW_I,        "YawI" },
+    // { TUNING_YAW_D,        "YawD" },
+    // { TUNING_YAW_FF,       "YawFF" },
     { TUNING_NONE, nullptr }
 };
 
@@ -220,6 +236,18 @@ AP_Float *AP_Tuning_Plane::get_param_pointer(uint8_t parm)
 
     case TUNING_PIT_FF:
         return &plane.pitchController.kFF();
+    
+    // case TUNING_YAW_P:
+    //     return &plane.yawController.kP();
+
+    // case TUNING_YAW_I:
+    //     return &plane.yawController.kI();
+
+    // case TUNING_YAW_D:
+    //     return &plane.yawController.kD();
+
+    // case TUNING_YAW_FF:
+    //     return &plane.yawController.kFF();
     }
     return nullptr;
 }
